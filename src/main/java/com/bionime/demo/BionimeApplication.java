cbd95e717a53f644c13227714b2abc3438edc9f6
andy.chen <yejh90093@gmail.com>
2020-05-21 14:34:35 +0800

    Initial commit

---
+++ src/main/java/com/bionime/demo/BionimeApplication.java
@@ -0,0 +1,13 @@
+package com.bionime.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BionimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BionimeApplication.class, args);
+	}
+
+}
